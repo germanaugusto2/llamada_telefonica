5eefd8321f4294a76c00dd36890d006ae12a47ba
sistemas <germanordonez@colegioguanenta.edu.co>
2023-02-17 10:18:09 -0500

    se modifico llamada_telefono.py

---
+++ llamada_telefonica.py
@@ -0,0 +1,10 @@
+# Programa para calclar el tiempo de la llamada 
+int(input("digie el valor de la llamada"))
+
+if minutos == < 3:
+     costo_llamada: 300
+
+else:
+    costo_llamada: 300 + 50* (mum_min - 3)
+
+print("el costo de la llamada es: ") + costo_llamada
